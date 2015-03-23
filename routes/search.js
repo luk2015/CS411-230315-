cd04a35a2a44fc57cb2caa9b3882d35cd2991625
luk2015 <luk@bu.edu>
2015-03-23 19:30:51 -0400

    CS411 v.2

---
+++ routes/search.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET search api. */
+router.get('/', function(req, res, next) {
+	var query = req.body.query;
+	var request = require('request');
+	request('http://api.eventful.com/json/events/search?app_key=SGjgGsbhWvJHdRxB&q=' + query + '&l=02134', function(error, response, body)
+            {
+		if(!error && response.statusCode == 200)
+            {
+			res.json(body)
+		}
+	})
+});
+
+
+module.exports = router;
+
+
